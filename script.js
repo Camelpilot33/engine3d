0582fd6883af0c6172ab6abb8d0a52fbdc05a0e4
Samuel Sastry <78936519+Camelpilot33@users.noreply.github.com>
2023-01-05 19:40:49 -0800

    render

--- script.js
+++ script.js
@@ -5,16 +5,9 @@ const zoom = 10
 const C = {w:canvas.width / zoom, h:canvas.height / zoom}
 const toDeg = x => x * 180 / Math.PI
 const toRad = x => x / 180 * Math.PI
+const C2V=(cx,cy)=>new vec(cx*V.w/C.w,cy*V.h/C.h,V.d)
 
-// x-axis: fwd   y-axis: right  z-axis: up
-function set(x, y, rgb=[0,0,0]) {
-	rgb=rgb.map(e=>e*0xFF)
-	ctx.fillStyle = `rgb(${rgb.toString()})`
-	ctx.fillRect(x * zoom, y * zoom, zoom, zoom);
-}
-function clear() {
-	ctx.clearRect(0, 0, C.w, C.h);
-}
+//classes
 class vec {
 	constructor(x, y, z) {
 		this.x = x || 0
@@ -58,15 +51,93 @@ class vec {
 		return a.x * b.x + a.y * b.y + a.z * b.z
 	}
 }
-
-let O = new vec(0, 0, 0)
-//viewport
-let V={w:1,h:1,d:1}
-const CtoV=(cx,cy)=>new vec(cx*V.w/C.w,cy*V.h/C.h,V.d)
-for (i=0;i<C.w;i++) {
-	for (j=0;j<C.h;j++) {
-		set(i,j,[((i*j)/10)%1,((i+j)/10)%1,((i**j)/(j**i))%1])
+class sphere {
+	constructor(pos,r,clr) {
+		this.pos=pos
+		this.r=r||1
+		this.color=clr||[0,0,0]
 	}
 }
+// draw
+function set(x, y, rgb=[0,0,0]) {
+	rgb=rgb.map(e=>e*0xFF)
+	ctx.fillStyle = `rgb(${rgb.toString()})`
+	ctx.fillRect((x+C.w/2-0.5) * zoom, (y+C.w/2-0.5) * zoom, zoom, zoom);
+}
+function clear() {
+	ctx.clearRect(0, 0, C.w, C.h);
+}
 
+
+//World
+let V={w:1,h:1,d:1}
+let O = new vec(0, 0, 0)
+
+let scene={
+	background:[0,0,0],
+	spheres:[
+		new sphere(new vec(3,0,10),1,[0,1,0]),
+		new sphere(new vec(-3,-4,10),1,[1,0,1]),
+		new sphere(new vec(0,0,10),0.8,[1,0,0]),
+		new sphere(new vec(-1,1,11),2.1,[1,1,1])
+	]
+}
+//ray tracing
+function trace(O, D, t_min, t_max) {
+    let closest_t = Infinity
+    let closest_sphere = null
+    for (sphere of scene.spheres) {
+		// console.log(sphere)
+        let [t1, t2] = IntersectRaySphere(O, D, sphere)
+		// console.log(t1,t2)
+        if (t_min<t1<t_max && t1 < closest_t) {
+            closest_t = t1
+            closest_sphere = sphere
+    	}
+        if (t_min<t2<t_max && t2 < closest_t) {
+            closest_t = t2
+            closest_sphere = sphere
+        }
+    }
+    if (closest_sphere == null) {
+       return scene.background
+    }
+	// console.log(closest_t)
+    return closest_sphere.color.map(e=>(1-(closest_t/10))*10*e)
+}
+function IntersectRaySphere(O, D, sphere) {
+	// console.warn(O,D,sphere)
+	let r = sphere.r
+    let CO = vec.add(O,vec.prod(sphere.pos,-1))
+
+    let a = vec.dot(D, D)
+    let b = 2*vec.dot(CO, D)
+    let c = vec.dot(CO, CO) - r*r
+
+    let discriminant = b*b - 4*a*c
+    if (discriminant < 0) {
+        return [Infinity,Infinity]
+    }
+    t1 = (-b + Math.sqrt(discriminant)) / (2*a)
+    t2 = (-b - Math.sqrt(discriminant)) / (2*a)
+	// console.log(t1,t2,b,discriminant,a)
+    return [t1, t2]
+}
+//test render
+console.log(C)
+function draw() {
+	for (x=-C.w/2;x<=C.w/2;x++) {
+		for (y=-C.h/2;y<=C.h/2;y++) {
+			//set(i,j,[((i*j)/10)%1,((i+j)/10)%1,(i/j*10)%1])
+			let D=C2V(x,y)
+			let color=trace(O,D,1,Infinity)
+			// console.log(x,y,D,color)
+			set(x,y,color)
+		}
+	}
+}
+setInterval(function(){
+	draw()
+	scene.spheres[0].pos.x-=0.01
+},10)
 console.info('Terminated without error')
